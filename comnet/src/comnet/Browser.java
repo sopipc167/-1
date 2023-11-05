1a87d3ff634ac99840cb91bcb578368bd0db0c10
Lee Jung Woo <prayslaks@naver.com>
2023-11-05 18:52:13 +0900

    위키피디아에 대해서 이미지와 타이틀을 불러올 수 있음

--- comnet/src/comnet/Browser.java
+++ comnet/src/comnet/Browser.java
@@ -2,53 +2,83 @@ package comnet;
 
 import java.awt.*;
 import java.awt.event.*;
-
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class Browser extends Frame implements ActionListener, WindowListener
 {
-	public Panel panel_Browsing;
-	public TextField textField_Browse;
-	public Button button_Browse;
-	public TextArea textArea_Result;
+	public JPanel panel_Browsing;
+	public JPanel panel_Result;
 	
-	public Browser()
+	public JTextField textField_Browse;
+	public JButton button_Browse;
+	
+	public JTextArea textArea_Result;
+	public JLabel label_Result;
+	 
+	
+	public Browser() throws IOException
 	{
 		super("COMNET_BROWSER");
 		
 		//검색 판넬
-		panel_Browsing = new Panel();
+		panel_Browsing = new JPanel();
 		
 		//검색 창
-		textField_Browse = new TextField(30);
+		textField_Browse = new JTextField(30);
 		
 		//검색 버튼
-		button_Browse = new Button("Browse");
+		button_Browse = new JButton("Browse");
 		button_Browse.addActionListener(this);
 		
-		//검색 결과
-		textArea_Result = new TextArea();
+		//검색 결과 판넬
+		panel_Result = new JPanel();
 		
+		//검색 결과 텍스트
+		textArea_Result = new JTextArea();
+		textArea_Result.setPreferredSize(new Dimension(500, 100));
+		
+		//검색 결과 이미지
+		label_Result = new JLabel();
+		label_Result.setPreferredSize(new Dimension(100, 100));
+		
+		//검색 판넬 구성
 		add(panel_Browsing, BorderLayout.NORTH);
-		add(textArea_Result, BorderLayout.CENTER);
 		panel_Browsing.add(textField_Browse);
 		panel_Browsing.add(button_Browse);
+		
+		//검색 결과 판넬 구성
+		add(panel_Result, BorderLayout.CENTER);
+		panel_Result.add(textArea_Result);
+		panel_Result.add(label_Result);
 		
 		//윈도우 이벤트
 		addWindowListener(this);
 		
 		//프레임 설정
-		setBounds(300, 300, 500, 500);
+		setBounds(300, 300, 1000, 1000);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation((dim.width/2)-(getWidth()/2), (dim.height/2)-(getHeight()/2));
 		setBackground(new Color(180,211,211));
 		setVisible(true);
+		
+		
+		
 	}
 	
 	public static void main(String[] args) throws Exception
@@ -119,13 +149,13 @@ public class Browser extends Frame implements ActionListener, WindowListener
 		}
 	}
 	
+	
 	public void SendHttpGetRequestMsg() throws Exception
 	{
 		//검색창으로부터 URL 획득
     	URL obj = new URL(textField_Browse.getText());
     	String host = obj.getHost();
     	String path = (obj.getPath()!="") ? obj.getPath() : "/";
-    	String protocol = obj.getProtocol();
     	
         /*와어어샤크 때 사용하던 DNS와 포트 80번 (Deprecated : HTTP용)
         Socket clientSocket = new Socket(host, 80); */
@@ -148,21 +178,39 @@ public class Browser extends Frame implements ActionListener, WindowListener
         //소켓을 통해서 HTTP GET Request 메시지 전송
         outToServer.writeBytes(HTTPGETRequestMsg);
         
-        //소켓을 통해서 HTTP GET Request 메시지 수신
-        String HTTPGETResponseMsg = "";
+        String imgURL = "";
+        String title = "";
         while(true)
         {
             String line = inFromServer.readLine();
 
-            System.out.println(line);
-            HTTPGETResponseMsg += line + "\n";
+            if((line.contains("jpg") || line.contains("png")) && imgURL.isBlank())
+            {
+            	String a = line.substring(35);
+            	String b = a.substring(0, a.length() - 2);
+            	imgURL = b;
+            }
+            else if(line.contains("title") && title.isBlank())
+            {
+            	title = line;
+            }
             
-            if(line.contains("</html>"))
-                break;
+            if(line.contains("</html>") || line.contains("</body>"))
+            	break;
         }
         
         //검색 결과 출력
-        textArea_Result.setText(HTTPGETResponseMsg);
+        textArea_Result.setText(title);
+        
+		//검색 결과 이미지의 크기 조정
+        URL url = new URL(imgURL);
+		ImageIcon originalImage = new ImageIcon(ImageIO.read(url));
+		Image ximg = originalImage.getImage();
+		Image yimg = ximg.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon scaledImage = new ImageIcon(yimg); 
+		
+		//검색 결과 이미지 출력
+		label_Result.setIcon(scaledImage);
 
         //소켓 종료
         clientSocket.close();
